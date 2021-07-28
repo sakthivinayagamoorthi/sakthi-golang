5380c63f8ed4b19d3cba6b5c45b79217200e1771
sakthivinayagamoorthi <sakthivinayagamoorthi2000@gmail.com>
2021-07-27 23:48:43 -0700

    jwt

---
+++ go-auth/go.mod
@@ -0,0 +1,9 @@
+module example/main.go
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber v1.14.6 // indirect
+	gorm.io/driver/mysql v1.1.1 // indirect
+	gorm.io/gorm v1.21.11 // indirect
+)
---
+++ go-auth/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go-auth/database"
+	"go-auth/routes"
+
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/middleware/cors"
+)
+
+func main() {
+	database.Connect()
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	routes.Setup(app)
+
+	app.Listen(":8000")
+}
